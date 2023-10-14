1e145862b14be389fd7fa7a9f2a861e7800db7e0
panth54 <90753170+panth54@users.noreply.github.com>
2023-10-15 00:40:08 +0700

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -9,6 +9,8 @@
 int readAndParse(FILE *, char *, char *, char *, char *, char *);
 int isNumber(char *);
 void dectobin(int dec, char *bin, int n);
+char* bintohex(const char* binary);
+int hextodec(const char* hex);
 
 typedef struct {
     char label[6];    
@@ -55,67 +57,54 @@ int main(int argc, char *argv[])
         count++;
     }
 
-    // printf("count da lines %d", count);
-
     rewind(inFilePtr);
     Code asb_code[count];
 
     for (int i = 0; i < count; i++) {
-    if (readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2)) {
-        if (strcmp(opcode, "add") == 0 || strcmp(opcode, "nand") == 0 ||
-            strcmp(opcode, "lw") == 0 || strcmp(opcode, "sw") == 0 ||
-            strcmp(opcode, "beq") == 0) {
-            // Process R-type and I-type instructions.
-            strcpy(asb_code[i].label, label);
-            strcpy(asb_code[i].opcode, opcode);
-            strcpy(asb_code[i].arg0, arg0);
-            strcpy(asb_code[i].arg1, arg1);
-            strcpy(asb_code[i].arg2, arg2);
-            asb_code[i].address = i;
-        } else if (strcmp(opcode, "jalr") == 0) {
-            // For "jalr" opcode, set arg2 to null character.
-            strcpy(asb_code[i].label, label);
-            strcpy(asb_code[i].opcode, opcode);
-            strcpy(asb_code[i].arg0, arg0);
-            strcpy(asb_code[i].arg1, arg1);
-            asb_code[i].arg2[0] = '\0'; // Set arg2 to null character
-            asb_code[i].address = i;
-        } else if (strcmp(opcode, "noop") == 0 || strcmp(opcode, "halt") == 0) {
-            // Process O-type instructions.
-            strcpy(asb_code[i].label, label);
-            strcpy(asb_code[i].opcode, opcode);
-            asb_code[i].arg0[0] = '\0'; // Set arg0 to null character
-            asb_code[i].arg1[0] = '\0'; // Set arg1 to null character
-            asb_code[i].arg2[0] = '\0'; // Set arg2 to null character
-            asb_code[i].address = i;
-       } else if (strcmp(opcode, ".fill") == 0) {
-            // Handle ".fill" instruction separately.
-            strcpy(asb_code[i].label, label);
-            strcpy(asb_code[i].opcode, opcode);
-            strcpy(asb_code[i].arg0, arg0);
-            asb_code[i].arg1[0] = '\0'; // Set arg1 to null character
-            asb_code[i].arg2[0] = '\0'; // Set arg2 to null character
-            asb_code[i].address = i;
-        } else {
-            // Handle unrecognized opcode.
-            fprintf(stderr, "Error: Unrecognized opcode '%s'\n", opcode);
-            exit(1);
+        if (readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2)) {
+            if (strcmp(opcode, "add") == 0 || strcmp(opcode, "nand") == 0 ||
+                strcmp(opcode, "lw") == 0 || strcmp(opcode, "sw") == 0 ||
+                strcmp(opcode, "beq") == 0) {
+                // Process R-type and I-type instructions.
+                strcpy(asb_code[i].label, label);
+                strcpy(asb_code[i].opcode, opcode);
+                strcpy(asb_code[i].arg0, arg0);
+                strcpy(asb_code[i].arg1, arg1);
+                strcpy(asb_code[i].arg2, arg2);
+                asb_code[i].address = i;
+            } else if (strcmp(opcode, "jalr") == 0) {
+                // For "jalr" opcode, set arg2 to null character.
+                strcpy(asb_code[i].label, label);
+                strcpy(asb_code[i].opcode, opcode);
+                strcpy(asb_code[i].arg0, arg0);
+                strcpy(asb_code[i].arg1, arg1);
+                asb_code[i].arg2[0] = '\0'; // Set arg2 to null character
+                asb_code[i].address = i;
+            } else if (strcmp(opcode, "noop") == 0 || strcmp(opcode, "halt") == 0) {
+                // Process O-type instructions.
+                strcpy(asb_code[i].label, label);
+                strcpy(asb_code[i].opcode, opcode);
+                asb_code[i].arg0[0] = '\0'; // Set arg0 to null character
+                asb_code[i].arg1[0] = '\0'; // Set arg1 to null character
+                asb_code[i].arg2[0] = '\0'; // Set arg2 to null character
+                asb_code[i].address = i;
+        } else if (strcmp(opcode, ".fill") == 0) {
+                // Handle ".fill" instruction separately.
+                strcpy(asb_code[i].label, label);
+                strcpy(asb_code[i].opcode, opcode);
+                strcpy(asb_code[i].arg0, arg0);
+                asb_code[i].arg1[0] = '\0'; // Set arg1 to null character
+                asb_code[i].arg2[0] = '\0'; // Set arg2 to null character
+                asb_code[i].address = i;
+            } else {
+                // Handle unrecognized opcode.
+                fprintf(stderr, "Error: Unrecognized opcode '%s'\n", opcode);
+                exit(1);
+            }
         }
     }
-}
-    for (int i = 0; i < count; i++) {
-        printf("Instruction %-2d", i );
-        printf("Label: %-6s", asb_code[i].label);
-        printf("Opcode: %-6s", asb_code[i].opcode);
-        printf("Arg0: %-6s", asb_code[i].arg0);
-        printf("Arg1: %-6s", asb_code[i].arg1);
-        printf("Arg2: %-6s", asb_code[i].arg2);
-        printf("Address: %-6d", asb_code[i].address);
-        printf("\n");
-    }
-    
-    char m_code[count][33];
 
+    char m_code[count][33];
     char field0[4];
     char field1[4];
     char field2[4];
@@ -144,49 +133,90 @@ int main(int argc, char *argv[])
                 {
                     if (!strcmp(asb_code[j].label,asb_code[i].arg2))
                     {
-                        dectobin(atoi(asb_code[j].arg0), offset, 16);
+                        dectobin(asb_code[j].address, offset, 16);
                     }
                 }
             }
             snprintf(m_code[i], sizeof(m_code[i]), "0000000010%s%s%s", field0, field1, offset);
         }else if (!strcmp(asb_code[i].opcode, "sw"))
         {
-            /* code */
+            dectobin(atoi(asb_code[i].arg0), field0, 3);
+            dectobin(atoi(asb_code[i].arg1), field1, 3);
+            if (isNumber(asb_code[i].arg2))
+            {
+                dectobin(atoi(asb_code[i].arg2), offset, 16);
+            }
+            else
+            {
+                for (int j = 0; j < count; j++)
+                {
+                    if (!strcmp(asb_code[j].label, asb_code[i].arg2))
+                    {
+                        dectobin(asb_code[j].address, offset, 16);
+                    }
+                }
+            }
+            snprintf(m_code[i], sizeof(m_code[i]), "0000000011%s%s%s", field0, field1, offset);
         }else if (!strcmp(asb_code[i].opcode, "beq"))
         {
-            /* code */
-        }else if (!strcmp(asb_code[i].opcode, "jalr"))
-        {
-            /* code */
-        }
-        else if (!strcmp(asb_code[i].opcode, "halt"))
-        {
-            /* code */
-        }else if (!strcmp(asb_code[i].opcode, "noop"))
-        {
-            /* code */
-        }else if (!strcmp(asb_code[i].opcode, ".fill"))
-        {
-            /* code */
-        }
-        
-        
-        
-        
-        
+            dectobin(atoi(asb_code[i].arg0), field0, 3);
+            dectobin(atoi(asb_code[i].arg1), field1, 3);
+            if (isNumber(asb_code[i].arg2))
+            {
+                dectobin(atoi(asb_code[i].arg2), offset, 16);
+            }
+            else
+            {
+                int relative_offset = 0;
 
-        
+                for (int j = 0; j < count; j++) {
+                    if (!strcmp(asb_code[j].label, asb_code[i].arg2)) {
+                        relative_offset = j - i - 1;
+                        break;
+                    }
+                }
+                
+                dectobin(relative_offset, offset, 16);
+            }
+            snprintf(m_code[i], sizeof(m_code[i]), "0000000100%s%s%s", field0, field1, offset);
+        }else if (!strcmp(asb_code[i].opcode, "jalr")) {
+            dectobin(atoi(asb_code[i].arg0), field0, 3);
+            dectobin(atoi(asb_code[i].arg1), field1, 3);
+            snprintf(m_code[i], sizeof(m_code[i]), "0000000101%s%s0000000000000000", field0, field1);
+        } else if (!strcmp(asb_code[i].opcode, "halt")) {
+            snprintf(m_code[i], sizeof(m_code[i]), "00000000110000000000000000000000");
+        } else if (!strcmp(asb_code[i].opcode, "noop")) {
+            snprintf(m_code[i], sizeof(m_code[i]), "00000001110000000000000000000000");
+        } else if (!strcmp(asb_code[i].opcode, ".fill")) {
+            // Check if the argument is a number or a label
+            if (isNumber(asb_code[i].arg0)) {
+                int value = atoi(asb_code[i].arg0);
+                char binaryValue[33];
+                dectobin(value, binaryValue, 32); // Convert the value to a 32-bit binary string
+                strcpy(m_code[i], binaryValue); // Store the binary value in the machine code array
+            } else {
+                // Handle the case where the argument is a label
+                // Find the label and get its corresponding value
+                for (int j = 0; j < count; j++) {
+                    if (!strcmp(asb_code[j].label, asb_code[i].arg0)) {
+                        int value = asb_code[j].address; // Assuming the address is the value for the label
+                        char binaryValue[33];
+                        dectobin(value, binaryValue, 32); // Convert the value to a 32-bit binary string
+                        strcpy(m_code[i], binaryValue); // Store the binary value in the machine code array
+                        break;
+                    }
+                }
+            }
+        }  
+
     }
 
     for (int i = 0; i < count; i++)
     {
-       printf("%s\n",m_code[i]);
+        char* hex = bintohex(m_code[i]);
+        int dec = hextodec(hex);
+        printf("(address %d): %d (hex 0x%s )\n",i ,dec , hex);
     }
-    
-    
-
-    
-
 
     return(0);
 }
@@ -247,15 +277,51 @@ int isNumber(char *string)
     return( (sscanf(string, "%d", &i)) == 1);
 }
 
-int sort(){
-    return 0;
+char* bintohex(const char* binary) {
+    int decimal = 0;
+    int binaryLength = strlen(binary);
+
+    // Convert binary to decimal
+    for (int i = 0; i < binaryLength; i++) {
+        if (binary[i] == '1') {
+            decimal += 1 << (binaryLength - 1 - i);
+        }
+    }
+
+    // Convert decimal to lowercase hexadecimal
+    char* hex = (char*)malloc(9);  // Assuming a 32-bit integer, so 8 characters maximum for hexadecimal
+    sprintf(hex, "%x", decimal);  // Use %x to format in lowercase
+
+    return hex;
+}
+
+int hextodec(const char* hex) {
+    int decimal = 0;
+    int hexLength = strlen(hex);
+
+    for (int i = 0; i < hexLength; i++) {
+        char c = hex[i];
+        int digit;
+        if (c >= '0' && c <= '9') {
+            digit = c - '0';
+        } else if (c >= 'A' && c <= 'F') {
+            digit = c - 'A' + 10;
+        } else if (c >= 'a' && c <= 'f') {
+            digit = c - 'a' + 10;
+        } else {
+            // Invalid character in the hexadecimal string
+            return -1;  // You can choose an appropriate error code
+        }
+        decimal = decimal * 16 + digit;
+    }
+
+    return decimal;
 }
 
 void dectobin(int dec, char *bin, int n) {
     for (int i = n - 1; i >= 0; i--) {
-        bin[i] = (dec & 1) + '0'; 
-        dec >>= 1; 
+        int bit = (dec >> i) & 1;  // Extract the i-th bit
+        bin[n - 1 - i] = bit ? '1' : '0';
     }
-    bin[n] = '\0';
+    bin[n] = '\0';  // Null-terminate the string
 }
-
